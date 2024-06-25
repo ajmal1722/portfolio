b13bc4a7260da142c7b08858cbb030b81da8e705
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-25 23:13:08 +0530

    bio updated

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,15 +1,25 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: `Exceptionally skilled Full-Stack developer with self-taught mastery of the MERN Stack. I
-			have a strong proficiency in both Back-End and Front-End development, Which I have
-			developed through hands-on experience, and a passion for continuous learning. I excel at
-			implementing cutting edge technologies to create innovative solutions that demonstrate a
-			deep understanding of modern web development
+  {
+    id: 1,
+    bio: `Hello! I'm Muhammed Ajmal, a passionate Full Stack Developer from 
+			Kozhikode, Kerala, India. My journey in web development has been fueled 
+			by a keen interest in creating dynamic and user-friendly web applications.
 		`,
-	},
-	{
-		id: 2,
-		bio: '',
-	},
+  },
+  {
+    id: 2,
+    bio: `I specialize in a robust set of technologies, including JavaScript, MongoDB, Express, Node.js, and React. My proficiency spans both front-end and back-end development, allowing me to build comprehensive solutions from start to finish. One of my significant projects is TryNutritions, an eCommerce platform designed to enhance user shopping experiences. This project, hosted at trynutritions.shop, utilizes Express and MongoDB for its backend and Bootstrap 5 for responsive design. In addition to TryNutritions, I've developed several other mini projects such as a React Kanban App, a task management tool inspired by the Kanban methodology; a React Seminar Hall Booking App, which simplifies the process of booking seminar halls; and a React Cart Manager, a streamlined shopping cart application.`,
+  },
+  {
+	id: 3,
+	bio: `My skills include JavaScript, MongoDB, Express, Node.js, and React. Additionally, I am proficient with tools like Postman for API testing, Git and GitHub for version control and collaboration, Vercel for deployment, and Figma for design and prototyping.`
+  },
+  {
+	id: 4,
+	bio: `My goal is to become an experienced Full Stack Developer, continuously expanding my knowledge and contributing to innovative projects. I am particularly passionate about working on scalable and impactful web applications that solve real-world problems. When I'm not coding, I enjoy [mention any hobbies or interests you have]. Being involved in the tech community and staying updated with the latest trends in web development are also important to me.`
+  },
+  {
+	id: 5,
+	bio: `Thank you for visiting my portfolio. Feel free to explore my projects and get in touch if you'd like to collaborate or learn more about my work.`
+  },
 ];
